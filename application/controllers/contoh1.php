d7b0d4c830317ca639427dac5d70305d32730bcd
gatpanramadhan <ghatfhanamadhan21@gmail.com>
2024-04-02 15:00:01 +0700

    tugas pertemuan 3

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,12 +1,12 @@
 <?php
 class Contoh1 extends CI_Controller
 {
- public function index()
- {
- echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Ghatfhan Ramadhan
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ghatfhan Ramadhan
  Saya tinggal di daerah Bekas
  olah raga yang saya sukai adalah
  Bulutangkis";
- }
+    }
 }
